ffded1e077c15e2abdfdb48634624eb8d0f7ec99
Nerzal <theel.tobias@gmx.de>
2014-11-09 16:00:08 +0100

    Moved project into repository

---
+++ desktop/src/com/noobygames/nerzal/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package com.noobygames.nerzal.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.noobygames.nerzal.ThreeDTest;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "GDX 3D Test";
+		config.width = 800;
+		config.height = 640;
+		
+		new LwjglApplication(new ThreeDTest(), config);
+	}
+}
