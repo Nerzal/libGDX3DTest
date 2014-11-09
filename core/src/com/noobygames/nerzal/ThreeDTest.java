1d43d6b4faead772f39c14eabefa0d70ba650cd8
Nerzal <theel.tobias@gmx.de>
2014-11-09 16:18:35 +0100

    Added a method to load a ship model instead of the crate

--- core/src/com/noobygames/nerzal/ThreeDTest.java
+++ core/src/com/noobygames/nerzal/ThreeDTest.java
@@ -3,6 +3,7 @@ package com.noobygames.nerzal;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
@@ -16,6 +17,7 @@ import com.badlogic.gdx.graphics.g3d.ModelBatch;
 import com.badlogic.gdx.graphics.g3d.ModelInstance;
 import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
@@ -29,6 +31,11 @@ public class ThreeDTest implements ApplicationListener {
 
 	@Override
 	public void create() {
+		//createGreenBox();
+		loadShipModel();
+	}
+	
+	private void createGreenBox(){
 		environment = new Environment();
         environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
         environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
@@ -51,14 +58,39 @@ public class ThreeDTest implements ApplicationListener {
 		instance = new ModelInstance(model);
 	}
 
+	private void loadShipModel(){
+		 modelBatch = new ModelBatch();
+	        environment = new Environment();
+	        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+	        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+	         
+	        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	        cam.position.set(1f, 1f, 1f);
+	        cam.lookAt(0,0,0);
+	        cam.near = 1f;
+	        cam.far = 300f;
+	        cam.update();
+	 
+	        ModelLoader loader = new ObjLoader();
+	        model = loader.loadModel(Gdx.files.internal("ship/ship.obj"));
+	        instance = new ModelInstance(model);
+	 
+	        camController = new CameraInputController(cam);
+	        Gdx.input.setInputProcessor(camController);
+	}
+	
+	public void renderGreenBox(){
+		modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+	}
+	
 	@Override
 	public void render() {
 		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 		 
-        modelBatch.begin(cam);
-        modelBatch.render(instance, environment);
-        modelBatch.end();
+        renderGreenBox();
 	}
 
 	@Override
