428ee39ee278e22ffbd6f4363e542ac61ed92e21
Nerzal <theel.tobias@gmx.de>
2014-11-09 16:37:48 +0100

    Using the AssetManager to render multiple instances of ship

--- core/src/com/noobygames/nerzal/ThreeDTest.java
+++ core/src/com/noobygames/nerzal/ThreeDTest.java
@@ -1,15 +1,13 @@
 package com.noobygames.nerzal;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.ModelLoader;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g3d.Environment;
 import com.badlogic.gdx.graphics.g3d.Material;
 import com.badlogic.gdx.graphics.g3d.Model;
@@ -20,6 +18,7 @@ import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.loader.ObjLoader;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.utils.Array;
 
 public class ThreeDTest implements ApplicationListener {
 	public PerspectiveCamera cam;
@@ -28,17 +27,60 @@ public class ThreeDTest implements ApplicationListener {
 	public Model model;
 	public ModelInstance instance;
 	private Environment environment;
+	public AssetManager assets;
+	public Array<ModelInstance> instances;
+	private boolean loading;
 
 	@Override
 	public void create() {
-		//createGreenBox();
-		loadShipModel();
+		// createGreenBox();
+		// loadShipModel();
+		instances = new Array<ModelInstance>();
+		assetManagerTest();
 	}
-	
-	private void createGreenBox(){
+
+	private void doneLoading() {
+		 Model ship = assets.get("ship/ship.obj", Model.class);
+	        for (float x = -5f; x <= 5f; x += 2f) {
+	            for (float z = -5f; z <= 5f; z += 2f) {
+	                ModelInstance shipInstance = new ModelInstance(ship);
+	                shipInstance.transform.setToTranslation(x, 0, z);
+	                instances.add(shipInstance);
+	            }
+	        }
+	        loading = false;
+	}
+
+	private void assetManagerTest() {
+		modelBatch = new ModelBatch();
 		environment = new Environment();
-        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f,
+				0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f,
+				-0.8f, -0.2f));
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight());
+		cam.position.set(7f, 7f, 7f);
+		cam.lookAt(0, 0, 0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
+
+		assets = new AssetManager();
+		assets.load("ship/ship.obj", Model.class);
+		loading = true;
+	}
+
+	private void createGreenBox() {
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f,
+				0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f,
+				-0.8f, -0.2f));
 		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(),
 				Gdx.graphics.getHeight());
 		cam.position.set(10f, 10f, 10f);
@@ -46,10 +88,10 @@ public class ThreeDTest implements ApplicationListener {
 		cam.near = 1f;
 		cam.far = 300f;
 		cam.update();
-		
+
 		camController = new CameraInputController(cam);
 		Gdx.input.setInputProcessor(camController);
-		
+
 		modelBatch = new ModelBatch();
 		ModelBuilder mb = new ModelBuilder();
 		model = mb.createBox(5f, 5f, 5f,
@@ -58,39 +100,48 @@ public class ThreeDTest implements ApplicationListener {
 		instance = new ModelInstance(model);
 	}
 
-	private void loadShipModel(){
-		 modelBatch = new ModelBatch();
-	        environment = new Environment();
-	        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
-	        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
-	         
-	        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-	        cam.position.set(1f, 1f, 1f);
-	        cam.lookAt(0,0,0);
-	        cam.near = 1f;
-	        cam.far = 300f;
-	        cam.update();
-	 
-	        ModelLoader loader = new ObjLoader();
-	        model = loader.loadModel(Gdx.files.internal("ship/ship.obj"));
-	        instance = new ModelInstance(model);
-	 
-	        camController = new CameraInputController(cam);
-	        Gdx.input.setInputProcessor(camController);
+	private void loadShipModel() {
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f,
+				0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f,
+				-0.8f, -0.2f));
+
+		cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(),
+				Gdx.graphics.getHeight());
+		cam.position.set(1f, 1f, 1f);
+		cam.lookAt(0, 0, 0);
+		cam.near = 1f;
+		cam.far = 300f;
+		cam.update();
+
+		ModelLoader loader = new ObjLoader();
+		model = loader.loadModel(Gdx.files.internal("ship/ship.obj"));
+		instance = new ModelInstance(model);
+
+		camController = new CameraInputController(cam);
+		Gdx.input.setInputProcessor(camController);
 	}
-	
-	public void renderGreenBox(){
+
+	public void renderGreenBox() {
 		modelBatch.begin(cam);
-        modelBatch.render(instance, environment);
-        modelBatch.end();
+		modelBatch.render(instance, environment);
+		modelBatch.end();
 	}
-	
+
 	@Override
 	public void render() {
-		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		 
-        renderGreenBox();
+		 if (loading && assets.update())
+	            doneLoading();
+	        camController.update();
+	         
+	        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+	 
+	        modelBatch.begin(cam);
+	        modelBatch.render(instances, environment);
+	        modelBatch.end();
 	}
 
 	@Override
@@ -113,7 +164,11 @@ public class ThreeDTest implements ApplicationListener {
 
 	@Override
 	public void dispose() {
-		model.dispose();
-
+		if (modelBatch != null)
+			modelBatch.dispose();
+		if (instances != null)
+			instances.clear();
+		if (assets != null)
+			assets.dispose();
 	}
 }
